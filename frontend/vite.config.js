0719b9095821efd17033692eae5c17a2034136e4
s4nt14go <santiagopergamo@gmail.com>
2025-03-17 18:40:12 -0300

    feat(front): remove package blocking the build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import configs from 'resolve-configuration';
 
 export default defineConfig({
-  plugins: [react(), configs({development: true})],
+  plugins: [react()],
   assetsInclude: ['**/*.mp3'], // Allow Vite to handle .mp3 files as static assets
   server: {
     hmr: {
